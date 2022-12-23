fd53afdd849adfc5f7ca591dcd6eb66f1f75c1c7
Rohan2627 <72944737+Rohan2627@users.noreply.github.com>
2022-12-23 14:18:48 +0000

    Add files via upload

---
+++ Project138/procode.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv('shared_articles.csv')
+df2 = pd.read_csv('users_interactions.csv')
+
+df1.columns = ['timestamp','eventType','contentId','authorPersonId', 'authorSessionId','authorUserAgent','authorRegion','authorCountry','contentType','url','title','text','lang']
+df2 = df2.merge(df1,on = 'contentId')
+
+# --------------------- Project 139 ---------------------------------------------------------
+
+
+df1 = df1[df1['eventType'] == "CONTENT SHARED"]
+
+def find_total_events(df1_row):
+  total_likes = df2[(df2["contentId"] == df1_row["contentId"]) & (df2["eventType"] == "LIKE")].shape[0]
+  total_views = df2[(df2["contentId"] == df1_row["contentId"]) & (df2["eventType"] == "VIEW")].shape[0]
+  total_bookmarks = df2[(df2["contentId"] == df1_row["contentId"]) & (df2["eventType"] == "BOOKMARK")].shape[0]
+  total_follows = df2[(df2["contentId"] == df1_row["contentId"]) & (df2["eventType"] == "FOLLOW")].shape[0]
+  total_comments = df2[(df2["contentId"] == df1_row["contentId"]) & (df2["eventType"] == "COMMENT CREATED")].shape[0]
+  return total_likes + total_views + total_bookmarks + total_follows + total_comments
+
+df1["total_events"] = df1.apply(find_total_events, axis=1)
+
+df1 = df1.sort_values(['total_events'] , ascending = False)
+
+print(df1.head(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
